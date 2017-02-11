83e06c6af942730bb6d11bdff593fe132e00c29e
jpeng304 <jspeng@csu.fullerton.edu>
2017-02-10 18:25:32 -0800

    Add files via upload

---
+++ SignUp.java
@@ -0,0 +1,17 @@
+package com.example.fitme.fitme;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jonathan on 2/5/2017.
+ */
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+}
